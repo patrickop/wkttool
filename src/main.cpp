dcf4ce67a3e2eff64aae0a34aec98999c15a85a4
Patrick <patrick.opgenoorth@gmail.com>
2020-07-19 01:51:31 +0100

    add input window

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 void draw(wkttool::SFMLWindowAdapter &window,
           const wkttool::geometry::Point &center, const float scale,
-          const wkttool::ScreenDimensions &dims) {
+          const wkttool::ScreenDimensions &dims, const std::string& shapes) {
   using namespace wkttool;
   window.clear(white);
   CoordinateBoundaries bounds{LowerXBoundary{x(center) - 10.0 * scale},
@@ -18,26 +18,32 @@ void draw(wkttool::SFMLWindowAdapter &window,
   ScreenProjection proj{dims, bounds};
   const auto grid = make_grid(bounds, XStep{2.0}, YStep{2.0});
   const auto axes = make_axes(bounds);
-  const auto samples =
-      subsample([](const double &x) { return std::sin(x); },
-                bounds.lower_x.get(), bounds.upper_x.get(), 800);
-  geometry::Polygon poly{
-      {geometry::Point{1, 1}, geometry::Point{1, 4}, geometry::Point{4, 5},
-       geometry::Point{4, 1}, geometry::Point{1, 1}},
-      {geometry::Point{2, 2}, geometry::Point{2, 3}, geometry::Point{3, 3},
-       geometry::Point{3, 2}, geometry::Point{2, 2}}};
-  const auto poly_segments = to_segments(poly);
-  const geometry::Linestring ls{geometry::Point{0, 0}, geometry::Point{33, 3},
-                                geometry::Point{44, 55}};
-  const auto ls_segments = to_segments(ls);
-  const geometry::Point pt{1, 1};
   window.draw(segments_to_drawables(grid, proj, grey, Thickness{1}));
   window.draw(segments_to_drawables(axes, proj, black, Thickness{2}));
-  window.draw(segments_to_drawables(samples, proj, black, Thickness{3}));
-  window.draw(segments_to_drawables(poly_segments, proj, black, Thickness{3}));
-  window.draw(segments_to_drawables(ls_segments, proj, red, Thickness{5}));
-  window.draw(
-      point_to_drawables(pt, proj, black, Thickness{1}, Right{10}, Down{10}));
+  //const auto samples =
+  //    subsample([](const double &x) { return std::sin(x); },
+  //              bounds.lower_x.get(), bounds.upper_x.get(), 800);
+  geometry::Polygon poly;
+  try {
+    boost::geometry::read_wkt(shapes, poly);
+    const auto poly_segments = to_segments(poly);
+    window.draw(segments_to_drawables(poly_segments, proj, black, Thickness{3}));
+  } catch (const std::exception& ex) {
+    std::cerr << ex.what() << std::endl;
+  }
+  //geometry::Polygon poly{
+  //    {geometry::Point{1, 1}, geometry::Point{1, 4}, geometry::Point{4, 5},
+  //     geometry::Point{4, 1}, geometry::Point{1, 1}},
+  //    {geometry::Point{2, 2}, geometry::Point{2, 3}, geometry::Point{3, 3},
+  //     geometry::Point{3, 2}, geometry::Point{2, 2}}};
+  //const geometry::Linestring ls{geometry::Point{0, 0}, geometry::Point{33, 3},
+  //                              geometry::Point{44, 55}};
+  //const auto ls_segments = to_segments(ls);
+  //const geometry::Point pt{1, 1};
+  //window.draw(segments_to_drawables(samples, proj, black, Thickness{3}));
+  //window.draw(segments_to_drawables(ls_segments, proj, red, Thickness{5}));
+  //window.draw(
+  //    point_to_drawables(pt, proj, black, Thickness{1}, Right{10}, Down{10}));
 
 }
 int main(int, char **) {
@@ -88,7 +94,7 @@ int main(int, char **) {
   while (running) {
     window.handle_events();
     if (redraw) {
-      draw(window, center, scale, dims);
+      draw(window, center, scale, dims, bla);
       // redraw = false;
     }
     const auto new_frame = std::chrono::system_clock::now();
@@ -97,6 +103,7 @@ int main(int, char **) {
 
     last_frame = new_frame;
     ImGui::Begin("Sample window"); // begin window
+    ImGui::GetClipboardText();
     ImGui::InputTextMultiline("Inputthing", &bla);
     is_window_hovered = ImGui::IsWindowHovered() or ImGui::IsWindowFocused();
     ImGui::End(); // end window

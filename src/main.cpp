0e154a6d53f6bdf014d35b4dcd5c99c646cdea95
Patrick <patrick.opgenoorth@gmail.com>
2020-07-26 22:37:47 +0100

    add window controls

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <wkttool/parse_geometry.h>
 #include <imgui.h>
 #include <imgui_stdlib.h>
+constexpr unsigned max_gridlines = 50;
+constexpr float min_coverage = 1e-5;
 constexpr auto initial_scenario = "POINT(1 2);\n\
 LINESTRING(0 0,2 2,3 1);\n\
 POLYGON((0 0,0 7,4 2,2 0,0 0));\n\
@@ -14,16 +16,18 @@ MULTILINESTRING((-1 2, -2 1),(7 7, 8 10));\n\
 MULTIPOLYGON(((0 0,0 -7,-4 -2,-2 0,0 0)), ((5 -2, 6 -3, 4 -2.5, 5 -2)))";
 template<class... Ts> struct overload : Ts... { using Ts::operator()...; };
 template<class... Ts> overload(Ts...) -> overload<Ts...>;
-
+using ImguiColor = std::array<float, 3>;
 struct Shape {
   std::string label;
   wkttool::geometry::Geometry geometry;
   wkttool::Thickness thickness;
-  wkttool::Color color;
   bool hovered;
   bool deleted;
   bool editing;
+  ImguiColor imgui_color;
 };
+
+
 
 struct BadLine {
   std::string error;
@@ -35,16 +39,52 @@ struct ImportResult {
   std::vector<BadLine> bad_lines;
 };
 
+struct GuiState {
+  std::vector<Shape> shapes;
+  ImguiColor background{1,1,1};
+  float center_x = 0;
+  float center_y = 0;
+  float coverage_x = 10;
+  float coverage_y = 10;
+  float grid_x = 2;
+  float grid_y = 2;
+  std::string editor_content = initial_scenario;
+  std::vector<std::string> errors;
+};
+
+void fix_impossibilities(GuiState& state) {
+  if (state.coverage_x == 0) state.coverage_x = min_coverage;
+  if (state.coverage_y == 0) state.coverage_y = min_coverage;
+  state.grid_x = std::fabs(state.grid_x);
+  state.grid_y = std::fabs(state.grid_y);
+  state.coverage_x = std::fabs(state.coverage_x);
+  state.coverage_y = std::fabs(state.coverage_y);
+  if (state.coverage_x / state.grid_x > max_gridlines) {
+    state.grid_x = state.coverage_x / (max_gridlines-1);
+  }
+  if (state.coverage_y / state.grid_y > max_gridlines) {
+    state.grid_y = state.coverage_y / (max_gridlines-1);
+  }
+}
+
+wkttool::Color to_color(const ImguiColor& imgui_color) {
+  return wkttool::Color{
+    wkttool::Red { static_cast<uint8_t>((255.0 * std::get<0>(imgui_color)))},
+    wkttool::Green { static_cast<uint8_t>((255.0 * std::get<1>(imgui_color)))},
+    wkttool::Blue { static_cast<uint8_t>((255.0 * std::get<2>(imgui_color)))}};
+}
+
 std::string generate_label() {
   static int counter = 0;
   return "shape" + std::to_string(counter++);
 }
 
-wkttool::Color random_color() {
-  return wkttool::Color{
-    wkttool::Red{static_cast<uint8_t>(rand()%255)},
-    wkttool::Green{static_cast<uint8_t>(rand()%255)},
-    wkttool::Blue{static_cast<uint8_t>(rand()%255)}};
+ImguiColor random_imgui_color() {
+  return {
+    ((float) rand() / (RAND_MAX)),
+    ((float) rand() / (RAND_MAX)),
+    ((float) rand() / (RAND_MAX))};
+
 }
 
 ImportResult import_shapes(const std::string& raw) {
@@ -56,7 +96,7 @@ ImportResult import_shapes(const std::string& raw) {
     try {
       const auto geo = parse(token);
       if (geo) {
-        result.shapes.push_back(Shape{generate_label(), *geo, Thickness{4}, random_color()});
+        result.shapes.push_back(Shape{generate_label(), *geo, Thickness{4}, false, false, false, random_imgui_color()});
       } else {
         result.bad_lines.push_back({"unidentified object type " + token, token});
       }
@@ -78,122 +118,77 @@ wkttool::ScreenProjection& proj) {
         std::visit(overload{
           [&] (const geometry::Polygon& poly) {
               const auto segments =  to_segments(poly);
-              window.draw(segments_to_drawables(segments, proj, shape.color, actual_thickness));},
+              window.draw(segments_to_drawables(segments, proj, to_color(shape.imgui_color), actual_thickness));},
           [&] (const geometry::MultiPolygon& polys) {
               const auto segments =  to_segments(polys);
-              window.draw(segments_to_drawables(segments, proj, shape.color, actual_thickness));},
+              window.draw(segments_to_drawables(segments, proj, to_color(shape.imgui_color), actual_thickness));},
           [&] (const geometry::MultiLinestring& lss) {
               const auto segments =  to_segments(lss);
-              window.draw(segments_to_drawables(segments, proj, shape.color, actual_thickness));},
+              window.draw(segments_to_drawables(segments, proj, to_color(shape.imgui_color), actual_thickness));},
           [&] (const geometry::Linestring& ls) {
               const auto segments =  to_segments(ls);
-              window.draw(segments_to_drawables(segments, proj, shape.color, actual_thickness));},
+              window.draw(segments_to_drawables(segments, proj, to_color(shape.imgui_color), actual_thickness));},
           [&] (const geometry::Point& point) {
               window.draw(point_to_drawables(
-              point, proj, shape.color, actual_thickness, Right{10}, Down{10}));},
+              point, proj, to_color(shape.imgui_color), actual_thickness, Right{10}, Down{10}));},
           [&] (const geometry::MultiPoint& points) {
               window.draw(points_to_drawables(
-              points, proj, shape.color, actual_thickness, Right{10}, Down{10}));}
+              points, proj, to_color(shape.imgui_color), actual_thickness, Right{10}, Down{10}));}
             }, shape.geometry);
 
   }
 }
-
-void draw(wkttool::SFMLWindowAdapter &window,
-          const wkttool::geometry::Point &center, const float scale,
-          const wkttool::ScreenDimensions &dims, const std::vector<Shape>& shapes) {
+wkttool::CoordinateBoundaries get_bounds(const GuiState& gui_state) {
   using namespace wkttool;
-  window.clear(white);
-  CoordinateBoundaries bounds{LowerXBoundary{x(center) - 10.0 * scale},
-                              LowerYBoundary{y(center) - 10.0 * scale},
-                              UpperXBoundary{x(center) + 10.0 * scale},
-                              UpperYBoundary{y(center) + 10.0 * scale}};
+  return CoordinateBoundaries {
+           LowerXBoundary{gui_state.center_x - (gui_state.coverage_x/2)},
+           LowerYBoundary{gui_state.center_y - (gui_state.coverage_y/2)},
+           UpperXBoundary{gui_state.center_x + (gui_state.coverage_x/2)},
+           UpperYBoundary{gui_state.center_y + (gui_state.coverage_y/2)}};
+}
+void draw_scene(wkttool::SFMLWindowAdapter &window, const GuiState& gui_state,
+          const wkttool::ScreenDimensions &dims) {
+  using namespace wkttool;
+  window.clear(to_color(gui_state.background));
+  const auto bounds = get_bounds(gui_state);
   ScreenProjection proj{dims, bounds};
-  const auto grid = make_grid(bounds, XStep{2.0}, YStep{2.0});
+  const auto grid = make_grid(bounds, XStep{gui_state.grid_x}, YStep{gui_state.grid_y});
   const auto axes = make_axes(bounds);
   window.draw(segments_to_drawables(grid, proj, grey, Thickness{1}));
   window.draw(segments_to_drawables(axes, proj, black, Thickness{2}));
-  draw_shapes(shapes, window, proj);
+  draw_shapes(gui_state.shapes, window, proj);
 
 }
-int main(int, char **) {
-  using namespace wkttool;
-  const ScreenDimensions dims{Right{800}, Down{600}};
-
-  SFMLWindowAdapter window{"my window",
-                           ScreenDimensions{Right{800}, Down{600}}};
-
-  bool running = true;
-  float scale = 1.0;
-  bool redraw = true;
-  bool is_window_hovered = false;
-  std::optional<ScreenLocation> mouse_pressed_down_at = std::nullopt;
-  geometry::Point center{0, 0};
-  window.connect([&running](const WindowClosed &) { running = false; });
-  window.connect([&scale, &redraw, &is_window_hovered](const MouseWheelScrolled &ev) {
-    if (is_window_hovered) return;
-    scale = scale * std::pow(1.20, -ev.amount.get());
-    redraw = true;
-  });
-  window.connect([&mouse_pressed_down_at, &is_window_hovered](const MouseButtonDown &ev) {
-    mouse_pressed_down_at = ev.location;
-  });
-  window.connect([&mouse_pressed_down_at, &is_window_hovered](const MouseButtonUp &ev) {
-    mouse_pressed_down_at = std::nullopt;
-  });
-  window.connect([&mouse_pressed_down_at, &redraw, &center, &scale,
-                  &dims, &is_window_hovered](const MouseMoved &ev) {
-    if (is_window_hovered) return;
-    if (mouse_pressed_down_at) {
-      const auto shift = *mouse_pressed_down_at - ev.destination;
-      CoordinateBoundaries bounds{LowerXBoundary{x(center) - 10.0 * scale},
-                                  LowerYBoundary{y(center) - 10.0 * scale},
-                                  UpperXBoundary{x(center) + 10.0 * scale},
-                                  UpperYBoundary{y(center) + 10.0 * scale}};
-      ScreenProjection proj{dims, bounds};
-      center = proj.translate(center, shift);
-      std::cout << "Shift by " << shift << std::endl;
-      std::cout << "Center moved to " << boost::geometry::wkt(center)
-                << std::endl;
-      mouse_pressed_down_at = ev.destination;
-      redraw = true;
-    }
-  });
-  auto last_frame = std::chrono::system_clock::now();
-  std::string editor {initial_scenario};
-  std::string errors;
-  std::vector<Shape> shapes;
-  while (running) {
-    window.handle_events();
-    draw(window, center, scale, dims, shapes);
-    const auto new_frame = std::chrono::system_clock::now();
-    std::stringstream framestring;
-    framestring << "Frame Time: " << (new_frame - last_frame).count() / 1e6;
-
-    last_frame = new_frame;
-    ImGui::Begin("Control");
-    ImGui::GetClipboardText();
-    ImGui::Text(framestring.str().c_str());
-    ImGui::InputTextMultiline("##Editor", &editor);
-    is_window_hovered = ImGui::IsWindowHovered() or ImGui::IsWindowFocused();
+void draw_gui(GuiState& gui_state) {
+    ImGui::Begin("Window Control");
+    ImGui::InputFloat("CenterX", &gui_state.center_x);
+    ImGui::InputFloat("CenterY", &gui_state.center_y);
+    ImGui::InputFloat("CoverageX", &gui_state.coverage_x);
+    ImGui::InputFloat("CoverageY", &gui_state.coverage_y);
+    ImGui::InputFloat("GridX", &gui_state.grid_x);
+    ImGui::InputFloat("GridY", &gui_state.grid_y);
+    ImGui::ColorEdit3("Background", (float*)gui_state.background.data(), ImGuiColorEditFlags_NoInputs );
+    ImGui::End();
+    ImGui::Begin("Scene");
+    ImGui::InputTextMultiline("##Editor", &gui_state.editor_content);
     if (ImGui::Button("Import")) {
-      const auto result = import_shapes(editor);
-      editor.clear();
-      errors.clear();
+      const auto result = import_shapes(gui_state.editor_content);
+      gui_state.editor_content.clear();
+      gui_state.errors.clear();
       std::copy(std::begin(result.shapes), std::end(result.shapes), 
-                std::back_inserter(shapes));
-      std::stringstream errors_stream;
+                std::back_inserter(gui_state.shapes));
       std::stringstream editor_stream;
       for (const auto& bad_line : result.bad_lines){
-        errors_stream << bad_line.error << "\n";
+        gui_state.errors.push_back(bad_line.error);
         editor_stream << bad_line.line << ";\n";
       }
-      editor = editor_stream.str();
-      errors = errors_stream.str();
+      gui_state.editor_content = editor_stream.str();
     }
     ImGui::Text("Errors:");
-    ImGui::Text(errors.c_str());
-    for (auto& shape : shapes) {
+    for (const auto& error : gui_state.errors) {
+      ImGui::Text(error.c_str());
+    }
+    for (auto& shape : gui_state.shapes) {
       ImGui::BeginGroup();
       ImGui::Text(shape.label.c_str());
       ImGui::SameLine();
@@ -204,6 +199,8 @@ int main(int, char **) {
       if (ImGui::Button((std::string{"Edit##"} + shape.label).c_str())) {
         shape.editing = true;
       }
+      ImGui::SameLine();
+      ImGui::ColorEdit3((std::string{"##Color"} + shape.label).c_str(), (float*)shape.imgui_color.data(), ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel );
       ImGui::EndGroup();
       if (ImGui::IsItemHovered()) {
         shape.hovered = true;
@@ -211,24 +208,86 @@ int main(int, char **) {
         shape.hovered = false;
       }
     }
-    auto it = std::begin(shapes);
-    while (it != std::end(shapes))
+    auto it = std::begin(gui_state.shapes);
+    while (it != std::end(gui_state.shapes))
     {
       if (it->deleted){
-        it = shapes.erase(it);
+        it = gui_state.shapes.erase(it);
       } else if (it->editing) {
         std::stringstream as_wkt;
-        as_wkt << ";\n";
+        as_wkt << "\n";
         std::visit( [&as_wkt](const auto& g) {
             as_wkt << boost::geometry::wkt(g);}, 
           it->geometry);
-        editor += as_wkt.str();
-        it = shapes.erase(it);
+        as_wkt << ";";
+        gui_state.editor_content += as_wkt.str();
+        it = gui_state.shapes.erase(it);
       }else{
         ++it;
         }
     }
     ImGui::End();
+}
+
+int main(int, char **) {
+  using namespace wkttool;
+  const ScreenDimensions dims{Right{800}, Down{600}};
+
+  SFMLWindowAdapter window{"my window",
+                           ScreenDimensions{Right{800}, Down{600}}};
+
+  bool running = true;
+  float scale = 1.0;
+  bool redraw = true;
+  bool disable_mouse = false;
+  GuiState gui_state;
+  std::optional<ScreenLocation> mouse_pressed_down_at = std::nullopt;
+  geometry::Point center{0, 0};
+  window.connect([&running](const WindowClosed &) { running = false; });
+  window.connect([&gui_state, &redraw, &disable_mouse](const MouseWheelScrolled &ev) {
+    if (disable_mouse) return;
+    gui_state.coverage_x *= std::pow(1.20, -ev.amount.get());
+    gui_state.coverage_y *= std::pow(1.20, -ev.amount.get());
+    redraw = true;
+  });
+  window.connect([&mouse_pressed_down_at, &disable_mouse](const MouseButtonDown &ev) {
+    mouse_pressed_down_at = ev.location;
+  });
+  window.connect([&mouse_pressed_down_at, &disable_mouse](const MouseButtonUp &ev) {
+    mouse_pressed_down_at = std::nullopt;
+  });
+  window.connect([&mouse_pressed_down_at, &redraw, &gui_state,
+                  &dims, &disable_mouse](const MouseMoved &ev) {
+    if (disable_mouse) return;
+    if (mouse_pressed_down_at) {
+      const auto shift = *mouse_pressed_down_at - ev.destination;
+      const auto bounds = get_bounds(gui_state);
+      ScreenProjection proj{dims, bounds};
+      const auto center = proj.translate(geometry::Point{gui_state.center_x, gui_state.center_y}, shift);
+      gui_state.center_x = boost::geometry::get<0>(center);
+      gui_state.center_y = boost::geometry::get<1>(center);
+      mouse_pressed_down_at = ev.destination;
+      redraw = true;
+    }
+  });
+  auto last_frame = std::chrono::system_clock::now();
+  std::string editor {initial_scenario};
+  std::string errors;
+  std::vector<Shape> shapes;
+  float x = 0;
+  float y = 0;
+  while (running) {
+    window.handle_events();
+    fix_impossibilities(gui_state);
+    draw_scene(window, gui_state, dims);
+    const auto new_frame = std::chrono::system_clock::now();
+    std::stringstream framestring;
+    framestring << "Frame Time: " << (new_frame - last_frame).count() / 1e6;
+    ImGui::GetClipboardText();
+
+    draw_gui(gui_state);
+    disable_mouse = ImGui::GetIO().WantCaptureMouse;
+    last_frame = new_frame;
     window.display();
   }
 
